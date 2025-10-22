d4283912a4d5517fcac8962d08d5553ea4e6eddd
Tanmay Tyagi <154615377+Tanmay-0101@users.noreply.github.com>
2025-10-23 03:35:38 +0530

    Implement Union-Find data structure in DSUClass.cpp

---
+++ DSUClass.cpp
@@ -0,0 +1,31 @@
+class UnionFind{
+	private: vector<int> par,rank;
+	public:
+		UnionFind(int n){
+			par.assign(n,0);
+			rank.assign(n,0);
+			iota(par.begin(),par.end(),0) //fills increasing values starting from 0
+		}
+
+		int find_set(int x){
+			return (par[x]==x ? x : par[x]=find_set(par[x]));
+		}
+
+		bool isEqualSet(int a,int b){
+			return find_set(a)==find_set(b);
+		}
+
+		void union(int u, int v){
+			if(!isEqualSet(u,v)){
+				int a=find_set(u),b=find_set(v);
+				if(rank[a]<rank[b]){
+					par[a]=b;
+				}else{
+					par[b]=a;
+					if(rank[a]=rank[b]){
+						rank[a]++;
+					}
+				}
+			}
+		}
+}
